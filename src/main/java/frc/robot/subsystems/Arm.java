874d13225ff32f88870b68bfddbde18f693d10e9
Nathan Baugh <nathanbaugh@frc4322.com>
2021-02-15 16:48:34 -0800

    Simplified arm code

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,11 +7,11 @@
 
 package frc.robot.subsystems;
 
-import com.fasterxml.jackson.annotation.JacksonInject.Value;
+// import com.fasterxml.jackson.annotation.JacksonInject.Value;
 import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
+// import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
+// import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -28,32 +28,32 @@ public class Arm extends SubsystemBase {
   private CANEncoder leftArm_encoder;
   private CANEncoder rightArm_encoder;
 
-  private CANPIDController ArmPidController;
+  // private CANPIDController ArmPidController;
   
   public Arm() {
       leftArm = new CANSparkMax(Constants.ArmConstants.leftMotor_ID, MotorType.kBrushless);
       rightArm = new CANSparkMax(Constants.ArmConstants.rightMotor_ID, MotorType.kBrushless);
 
-      leftArm_encoder = new CANEncoder(leftArm);
-      rightArm_encoder = new CANEncoder(rightArm);
+      // leftArm_encoder = new CANEncoder(leftArm);
+      // rightArm_encoder = new CANEncoder(rightArm);
 
-      ArmPidController = rightArm.getPIDController(); 
+      // ArmPidController = rightArm.getPIDController(); 
 
       rightArm.follow(leftArm, true);
 
-      ArmPidController.setP(Constants.ArmConstants.PID_Values.kP);
-      ArmPidController.setI(Constants.ArmConstants.PID_Values.kI);
-      ArmPidController.setD(Constants.ArmConstants.PID_Values.kD);
-      ArmPidController.setIZone(Constants.ArmConstants.PID_Values.kIz);
-      ArmPidController.setFF(Constants.ArmConstants.PID_Values.kFF);
-      ArmPidController.setOutputRange(Constants.ArmConstants.PID_Values.kMinOutput,Constants.ArmConstants.PID_Values.kMaxOutput);
+      // ArmPidController.setP(Constants.ArmConstants.PID_Values.kP);
+      // ArmPidController.setI(Constants.ArmConstants.PID_Values.kI);
+      // ArmPidController.setD(Constants.ArmConstants.PID_Values.kD);
+      // ArmPidController.setIZone(Constants.ArmConstants.PID_Values.kIz);
+      // ArmPidController.setFF(Constants.ArmConstants.PID_Values.kFF);
+      // ArmPidController.setOutputRange(Constants.ArmConstants.PID_Values.kMinOutput,Constants.ArmConstants.PID_Values.kMaxOutput);
 
-      int smartmotionslot = 0;
-      ArmPidController.setSmartMotionMaxVelocity(Constants.ArmConstants.PID_Values.maxVelocity, smartmotionslot);
-      ArmPidController.setSmartMotionMinOutputVelocity(Constants.ArmConstants.PID_Values.minVelocity, smartmotionslot);
-      ArmPidController.setSmartMotionMaxAccel(Constants.ArmConstants.PID_Values.maxAcceleration, smartmotionslot);
+      // int smartmotionslot = 0;
+      // ArmPidController.setSmartMotionMaxVelocity(Constants.ArmConstants.PID_Values.maxVelocity, smartmotionslot);
+      // ArmPidController.setSmartMotionMinOutputVelocity(Constants.ArmConstants.PID_Values.minVelocity, smartmotionslot);
+      // ArmPidController.setSmartMotionMaxAccel(Constants.ArmConstants.PID_Values.maxAcceleration, smartmotionslot);
     
-      ArmPidController.setSmartMotionAllowedClosedLoopError(Constants.ArmConstants.PID_Values.allowed_error, smartmotionslot);
+      // ArmPidController.setSmartMotionAllowedClosedLoopError(Constants.ArmConstants.PID_Values.allowed_error, smartmotionslot);
   
   }
 
@@ -70,25 +70,25 @@ public class Arm extends SubsystemBase {
     SmartDashboard.putNumber("Arm Encoder Velocity", getArmEncoderVelocity());
   }
 
-  public void reachCollectPosition()
-  {
-    ArmPidController.setReference(Constants.ArmConstants.collectSetpoint, ControlType.kSmartMotion);
-  }
+  // public void reachCollectPosition()
+  // {
+  //   ArmPidController.setReference(Constants.ArmConstants.collectSetpoint, ControlType.kSmartMotion);
+  // }
 
   public void set(double power)
   {
     leftArm.set(power);
   }
 
-  public void reachStartingConfiguration()
-  {
-    ArmPidController.setReference(Constants.ArmConstants.startingConfigSetpoint, ControlType.kSmartMotion);
-  }
+  // public void reachStartingConfiguration()
+  // {
+  //   ArmPidController.setReference(Constants.ArmConstants.startingConfigSetpoint, ControlType.kSmartMotion);
+  // }
 
-  public void reachClimbPosition()
-  {
-    ArmPidController.setReference(Constants.ArmConstants.climbSetpoint, ControlType.kSmartMotion);
-  }
+  // public void reachClimbPosition()
+  // {
+  //   ArmPidController.setReference(Constants.ArmConstants.climbSetpoint, ControlType.kSmartMotion);
+  // }
 
 
   @Override

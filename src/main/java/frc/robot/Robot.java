2939688a3bcecd82f398f0f647c48228d574dba0
Zayd Khan <zaydbkhan@gmail.com>
2021-06-24 15:55:45 -0700

    added encoder values to smart dashboard and added hood homing function

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -51,6 +52,8 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("hoodEncoder", m_robotContainer.shooterHood.getPosition());
+    m_robotContainer.shooterHood.clearEncoder();
     // SmartDashboard.putNumber("l_encoder_rate", m_drivebase.getLeftEncoders_Velocity());
     // SmartDashboard.putNumber("l_encoder_pos", m_drivebase.getLeftEncoders_Velocity());
     // SmartDashboard.putNumber("r_encoder_rate", m_drivebase.getRightEncoders_Velocity());

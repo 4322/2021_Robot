423002758cae14a37ef6d531616828750db65a8a
Ryan Vu <ryanvu@frc4322.com>
2022-01-22 12:54:38 -0800

    Created Firing Solution

---
+++ src/main/java/frc/robot/FiringSolution/FiringSolution.java
@@ -0,0 +1,24 @@
+package frc.robot.FiringSolution;
+
+public class FiringSolution {
+        private int flywheelSpeed;
+        private int hoodPosition;
+        private double distance;
+
+
+        public FiringSolution(int flywheelSpeed, int hoodPosition, double distance) {
+            this.flywheelSpeed = flywheelSpeed;
+            this.hoodPosition = hoodPosition;
+            this.distance = distance;
+
+
+        }
+
+        public int getflywheelSpeed() { return flywheelSpeed; }
+        public int gethoodPosition() { return hoodPosition; }
+        
+        public double getDistance() {
+            return distance;
+        }
+ 
+}
